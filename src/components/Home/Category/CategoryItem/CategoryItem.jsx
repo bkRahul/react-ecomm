b74cc966c0715f502ecb5bcfb41fef5d761b655a
bkRahul <rahulsatya1068@gmail.com>
2020-10-26 01:16:05 +0530

    added routing to homepage collection

--- src/components/Home/Category/CategoryItem/CategoryItem.jsx
+++ src/components/Home/Category/CategoryItem/CategoryItem.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 import classes from './CategoryItem.module.scss';
 
-const CategoryItem = ({ title, imageUrl, size }) => (
+const CategoryItem = ({ title, imageUrl, linkUrl, size, history, match }) => (
   <div
     className={`${size === 'large' ? classes.large : ''} ${
       classes.Category_item
     }`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
   >
     <div
       className={classes.background_image}
@@ -21,4 +23,4 @@ const CategoryItem = ({ title, imageUrl, size }) => (
   </div>
 );
 
-export default CategoryItem;
+export default withRouter(CategoryItem);
