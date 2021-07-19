15a36b35496272321e5ae995aa20a3c6c12d8955
bkRahul <rahulkrishnappa94@gmail.com>
2021-07-19 23:33:06 +0530

    added memo to cart item component

--- client/src/components/Header/Cart/CartDropdownItem/CartDropdownItem.jsx
+++ client/src/components/Header/Cart/CartDropdownItem/CartDropdownItem.jsx
@@ -1,22 +1,24 @@
-import React from 'react';
+import React, { memo } from 'react'
 import {
-  CartDropdownItemContainer,
-  Image,
-  ItemDetails,
-  Name,
-  Info,
-} from './CartDropdownItem.styles';
+	CartDropdownItemContainer,
+	Image,
+	ItemDetails,
+	Name,
+	Info,
+} from './CartDropdownItem.styles'
 
-export const CartDropdownItem = ({ item: { imageUrl, name, price, qty } }) => {
-  return (
-    <CartDropdownItemContainer>
-      <Image src={imageUrl} alt="product" />
-      <ItemDetails>
-        <Name>{name}</Name>
-        <Info>
-          {qty} &#10005; ${price}
-        </Info>
-      </ItemDetails>
-    </CartDropdownItemContainer>
-  );
-};
+export const CartDropdownItem = memo(
+	({ item: { imageUrl, name, price, qty } }) => {
+		return (
+			<CartDropdownItemContainer>
+				<Image src={imageUrl} alt='product' />
+				<ItemDetails>
+					<Name>{name}</Name>
+					<Info>
+						{qty} &#10005; ${price}
+					</Info>
+				</ItemDetails>
+			</CartDropdownItemContainer>
+		)
+	},
+)
