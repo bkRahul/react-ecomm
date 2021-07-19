17ab70549c66395f22adb4398a844e2d5edc8c21
bkRahul <rahulkrishnappa94@gmail.com>
2021-07-20 00:07:00 +0530

    resolved compose enhancers is not a function

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -6,9 +6,9 @@ import rootReducer from './root-reducer'
 import rootSaga from './root-saga'
 
 const composeEnhancers =
-	process.env.NODE_ENV === 'development'
+	(process.env.NODE_ENV === 'development'
 		? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-		: null || compose
+		: null) || compose
 
 const sagaMiddleware = createSagaMiddleware()
 
