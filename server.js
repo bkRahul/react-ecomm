6592d8f3a575f05a1ce26245085ac12487b46d89
bkRahul <rahulkrishnappa94@gmail.com>
2021-08-07 02:17:43 +0530

    added Pwa and compression

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const path = require("path");
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require('path');
+const compression = require('compression');
+const enforce = require('express-sslify');
 
-if (process.env.NODE_ENV !== "production") require("dotenv").config();
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+const port = process.env.PORT || 5000;
 
 const app = express();
 
@@ -17,28 +21,31 @@ app.use(
 );
 
 app.use(cors());
+app.use(compression());
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
 //serve static files
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
-
-const port = process.env.PORT || 5000;
 
 app.listen(port, (error) => {
   if (error) throw error;
-  console.log("server is running on port", port);
+  console.log('server is running on port', port);
 });
 
-app.post("/payment", (req, res) => {
-  console.log("req", req.body);
+app.get('/service-worker', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'service-worker.js'));
+});
+
+app.post('/payment', (req, res) => {
   const body = {
     source: req.body.token.id,
     amount: req.body.amount,
-    description: "Testing Payment",
-    currency: "inr",
+    description: 'Testing Payment',
+    currency: 'inr',
   };
 
   stripe.charges.create(body, (stripeErr, stripeRes) => {
